3cfa20b15b1452d9a9cf47915456b996c209e24b
Winter Lau <ld@oschina.net>
2014-01-07 11:43:02 +0800

    完善测试客户端

--- src/net/oschina/j2cache/CacheTester.java
+++ src/net/oschina/j2cache/CacheTester.java
@@ -30,25 +30,42 @@ public class CacheTester {
 	            String[] cmds = StringUtils.split(line);
 	            if("get".equalsIgnoreCase(cmds[0])){
 	            	String value = cache.get(cmds[1], cmds[2]);
-	            	System.out.println(value);
+	            	System.out.printf("[%s,%s]=>%s\n",cmds[1], cmds[2], value);
 	            }
 	            else
 	            if("set".equalsIgnoreCase(cmds[0])){
 	            	cache.set(cmds[1], cmds[2],cmds[3]);
-	            	System.out.println("done!");
+	            	System.out.printf("[%s,%s]<=%s\n",cmds[1], cmds[2], cmds[3]);
 	            }
 	            else
 	            if("evict".equalsIgnoreCase(cmds[0])){
 	            	cache.evict(cmds[1], cmds[2]);
-	            	System.out.println("evicted!");
+	            	System.out.printf("[%s,%s]=>null\n",cmds[1], cmds[2]);
 	            }
 	            else
+	            if("help".equalsIgnoreCase(cmds[0])){
+	            	printHelp();
+	            }
+	            else{
 	            	System.out.println("Unknown command.");
+	            	printHelp();
+	            }
+	        }
+	        catch(ArrayIndexOutOfBoundsException e) {
+            	System.out.println("Wrong arguments.");
+	        	printHelp();
 	        }
 	        catch(Exception e) {
 	        	e.printStackTrace();
 	        }
 	    }while(true);
+	    
+	    System.exit(0);
+	}
+	
+	private static void printHelp() {
+		System.out.println("Usage: [cmd] region key [value]");
+		System.out.println("cmd: get/set/evict/quit/exit/help");
 	}
 
 }
